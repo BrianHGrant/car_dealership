f1d0e03a7c765e49512c123a33c56cbbd3a575d6
Brian Grant and Sebastian Prokuski <brian@>
2016-07-07 11:21:10 -0700

    fix syntax and spelling errors

--- app.rb
+++ app.rb
@@ -2,19 +2,42 @@ require('sinatra')
 require('sinatra/reloader')
 also_reload('lib/**/*.rb')
 require('./lib/vehicle')
-require('.lib/dealership')
+require('./lib/dealership')
 
 get('/') do
   erb(:index)
+end
+
+get('/vehicles/new') do
+  erb(:vehicles_form)
 end
 
 get('/dealerships/new') do
   erb(:dealerships_form)
 end
 
+get('/vehicles') do
+  @vehicles = Vehicle.all()
+  erb(:vehicles)
+end
+
 get('/dealerships') do
   @dealerships = Dealership.all()
   erb(:dealerships)
+end
+
+post('/vehicles') do
+  make = params.fetch('make')
+  model = params.fetch('model')
+  year = params.fetch('year')
+  color = params.fetch('color')
+  engine_size = params.fetch('engine_size')
+  number_of_doors = params.fetch('number_of_doors')
+  @vehicle = Vehicle.new({:make => make, :model => model, :year => year, :color => color, :engine_size => engine_size, :number_of_doors => number_of_doors})
+  @vehicle.save()
+  @dealership = Dealership.find(params.fetch('dealership_id').to_i())
+  @dealership.add_vehicle(@vehicle)
+  erb(:success)
 end
 
 post('/dealerships') do
@@ -29,26 +52,12 @@ get('/vehicles/:id') do
   erb(:vehicle)
 end
 
-get('dealerships/:id') do
+get('/dealerships/:id') do
   @dealership = Dealership.find(params.fetch('id').to_i())
   erb(:dealership)
 end
 
-get('dealerships/:id/vehicle/new') do
+get('/dealerships/:id/vehicles/new') do
   @dealership = Dealership.find(params.fetch('id').to_i())
   erb(:dealership_vehicles_form)
-end
-
-post('/vehicles') do
-  make = params.fetch('make')
-  model = params.fetch('model')
-  year = params.fetch('year')
-  color = params.fetch('color')
-  engine_size = params.fetch('engine_size')
-  number_of_doors = params.fetch('number_of_doors')
-  @vehicle = Vehicle.new(make, model, year, color, engine_size, number_of_doors)
-  @vehicle.save()
-  @dealership = Dealership.find(params.fetch('dealership_id').to_i())
-  @dealership.add_vehicle(@vehicle)
-  erb(:success)
 end
--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,15 +1,15 @@
 class Vehicle
-  attr_reader(:make, :model, :year, :color, :engine_size, :number_of_doors)
+  attr_reader(:make, :model, :year, :color, :engine_size, :number_of_doors, :id)
   @@vehicles = []
 
-  def initialize(attributes)
+  define_method(:initialize) do |attributes|
     @make = attributes.fetch(:make)
     @model = attributes.fetch(:model)
     @year = attributes.fetch(:year)
     @color = attributes.fetch(:color)
     @engine_size = attributes.fetch(:engine_size)
     @number_of_doors = attributes.fetch(:number_of_doors)
-
+    @id = @@vehicles.length.+(1)
   end
 
   define_singleton_method (:all) do
@@ -18,6 +18,16 @@ class Vehicle
 
   define_method (:save) do
     @@vehicles.push(self)
+  end
+
+  define_singleton_method(:find) do |id|
+    found_vehicle = nil
+    @@vehicles.each() do |vehicle|
+      if vehicle.id().eql?(id)
+        found_vehicle = vehicle
+      end
+    end
+    found_vehicle
   end
 
   define_singleton_method (:clear) do
